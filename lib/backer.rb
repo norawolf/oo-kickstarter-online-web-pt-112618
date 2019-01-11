a554f3bec12e39c8a7b57c35a84e7570c4d7ff01
norawolf <noraevogt@gmail.com>
2019-01-11 13:32:50 -0500

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,22 +1,18 @@
 require 'pry'
 class Backer
 
-  @@backed_projects = []
-
   attr_accessor :name
+  attr_reader :backed_projects
 
   def initialize(name)
     @name = name
-  end
-
-  def backed_projects
-    @@backed_projects
+    @backed_projects = []
   end
 
 #  1) Backer - More Advanced #back_project also adds the backer (self) to the project's backers array
 
   def back_project(project)
-    @@backed_projects << project
+    @backed_projects << project
     project.add_backer(self)
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,18 +1,16 @@
 class Project
-  @@backers = []
   attr_accessor :title
+  attr_reader :backers
 
   def initialize(title)
     @title = title
+    @backers = []
   end
 
-  def backers
-    @@backers
-  end
 
 #1) Project - More Advanced #add_backer also adds the project to the backer's backed_projects array
   def add_backer(backer)
-    @@backers << backer
+    @backers << backer
     backer.backed_projects << self
   end
 end
